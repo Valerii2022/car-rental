7cadb345d85f96631abc9b49ccb2de86bbf21b37
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2023-09-14 00:24:37 +0300

    add cards and modal styles

--- src/components/AdvertsList/AdvertsList.jsx
+++ src/components/AdvertsList/AdvertsList.jsx
@@ -1,72 +1,102 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchAdverts } from 'redux/operations';
 import { getAdverts } from 'redux/selectors';
 import css from './AdvertsList.module.css';
+import { ReactComponent as HeartIcon } from '../../image/heart.svg';
+import { Modal } from 'components/Modal';
 
 export const AdvertsList = () => {
   const { items, isLoading, error } = useSelector(getAdverts);
   const dispatch = useDispatch();
+  const [openModal, setOpenModal] = useState(false);
+  const [id, setId] = useState(null);
 
   useEffect(() => {
     dispatch(fetchAdverts());
   }, [dispatch]);
 
+  const handleBtnClick = e => {
+    setId(e.target.id);
+    setOpenModal(true);
+  };
+
   return (
-    <div className={css.container}>
-      <ul className={css.list}>
-        {isLoading && <b>Loading contacts...</b>}
-        {error && <b>{error}</b>}
-        {items.map(
-          ({
-            year,
-            id,
-            make,
-            model,
-            img,
-            rentalPrice,
-            type,
-            mileage,
-            rentalCompany,
-            accessories,
-            address,
-          }) => {
-            return (
-              <li className={css.itemList} key={id}>
-                <div className={css.imageWrapper}>
-                  <img
-                    className={css.image}
-                    src={img}
-                    alt=""
-                    // width="274"
-                    height="268"
-                  />
-                </div>
-                <div className={css.titleWrapper}>
-                  <p>
-                    {make}
-                    <span className={css.accent}> {model}, </span>
-                    {year}
-                  </p>
-                  <p>{rentalPrice}</p>
-                </div>
-                <ul className={css.optionsList}>
-                  <li className={css.optionsItem}>{address.split(',')[1]}</li>
-                  <li className={css.optionsItem}>{address.split(',')[2]}</li>
-                  <li className={css.optionsItem}>{rentalCompany}</li>
-                  <li className={css.optionsItem}>{type}</li>
-                  <li className={css.optionsItem}>{model}</li>
-                  <li className={css.optionsItem}>{mileage}</li>
-                  <li className={css.optionsItem}>{accessories[0]}</li>
-                </ul>
-                <button className={css.learnMoreBtn} type="button">
-                  Learn more
-                </button>
-              </li>
-            );
-          }
-        )}
-      </ul>
-    </div>
+    <>
+      <div className={css.container}>
+        <ul className={css.list}>
+          {isLoading && <b>Loading contacts...</b>}
+          {error && <b>{error}</b>}
+          {items.map(
+            ({
+              year,
+              id,
+              make,
+              model,
+              img,
+              rentalPrice,
+              type,
+              mileage,
+              rentalCompany,
+              accessories,
+              address,
+            }) => {
+              return (
+                <li className={css.itemList} key={id}>
+                  <div className={css.imageWrapper}>
+                    <HeartIcon
+                      className={css.heartIcon}
+                      width="18"
+                      height="18"
+                    />
+                    <img
+                      className={css.image}
+                      src={img}
+                      alt=""
+                      // width="274"
+                      height="268"
+                    />
+                  </div>
+                  <div className={css.titleWrapper}>
+                    <h2 className={css.title}>
+                      {make}
+                      <span className={css.accent}> {model}, </span>
+                      {year}
+                    </h2>
+                    <p>{rentalPrice}</p>
+                  </div>
+                  <div className={css.optionsListWrap}>
+                    <ul className={css.optionsList}>
+                      <li className={css.optionsItem}>
+                        {address.split(',')[1]}
+                      </li>
+                      <li className={css.optionsItem}>
+                        {address.split(',')[2]}
+                      </li>
+                      <li className={css.optionsItem}>{rentalCompany}</li>
+                    </ul>
+                    <ul className={css.optionsList}>
+                      <li className={css.optionsItem}>{type}</li>
+                      <li className={css.optionsItem}>{model}</li>
+                      <li className={css.optionsItem}>{mileage}</li>
+                      <li className={css.optionsItem}>{accessories[0]}</li>
+                    </ul>
+                  </div>
+                  <button
+                    id={id}
+                    className={css.learnMoreBtn}
+                    type="button"
+                    onClick={handleBtnClick}
+                  >
+                    Learn more
+                  </button>
+                </li>
+              );
+            }
+          )}
+        </ul>
+      </div>
+      {openModal && <Modal currentId={id} modalIsOpen={setOpenModal} />}
+    </>
   );
 };
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,133 @@
+import React, { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import css from './Modal.module.css';
+import { ReactComponent as ClosetIcon } from '../../image/close.svg';
+import { useSelector } from 'react-redux';
+import { getAdverts } from 'redux/selectors';
+import { nanoid } from 'nanoid';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export const Modal = ({ currentId, modalIsOpen }) => {
+  const { items } = useSelector(getAdverts);
+  const {
+    year,
+    id,
+    make,
+    model,
+    img,
+    description,
+    rentalPrice,
+    functionalities,
+    type,
+    mileage,
+    rentalCompany,
+    rentalConditions,
+    accessories,
+    fuelConsumption,
+    engineSize,
+    address,
+  } = items.find(item => item.id === Number(currentId));
+
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        modalIsOpen();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [modalIsOpen]);
+
+  const handleBackdropClick = event => {
+    if (event.currentTarget === event.target) {
+      modalIsOpen(false);
+    }
+  };
+
+  return createPortal(
+    <div className={css.overlay} onClick={handleBackdropClick}>
+      <div className={css.modal}>
+        <ClosetIcon
+          className={css.closeIcon}
+          width="24"
+          height="24"
+          onClick={handleBackdropClick}
+        />
+        <img
+          className={css.image}
+          src={img}
+          alt=""
+          width="461"
+          //   height="248"
+        />
+        <h2 className={css.title}>
+          {make}
+          <span className={css.accent}> {model}, </span>
+          {year}
+        </h2>
+        <div className={css.optionsListWrap}>
+          <ul className={css.optionsList}>
+            <li className={css.optionsItem}>{address.split(',')[1]}</li>
+            <li className={css.optionsItem}>{address.split(',')[2]}</li>
+            <li className={css.optionsItem}>Id: {id}</li>
+            <li className={css.optionsItem}>Year: {year}</li>
+            <li className={css.optionsItem}>Type: {type}</li>
+          </ul>
+          <ul className={css.optionsList}>
+            <li className={css.optionsItem}>
+              Fuel Consumption: {fuelConsumption}
+            </li>
+            <li className={css.optionsItem}>Engine Size: {engineSize}</li>
+          </ul>
+        </div>
+        <p className={css.description}>{description}</p>
+        <p className={css.subTitle}>Accessories and functionalities:</p>
+        <div className={css.optionsListWrap}>
+          <ul className={css.optionsList}>
+            {accessories.map(item => {
+              return (
+                <li key={nanoid()} className={css.optionsItem}>
+                  {item}
+                </li>
+              );
+            })}
+          </ul>
+          <ul className={css.optionsList}>
+            {functionalities.map(item => {
+              return (
+                <li key={nanoid()} className={css.optionsItem}>
+                  {item}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <p className={css.subTitle}>Rental Conditions: </p>
+        <ul className={css.conditionsList}>
+          {rentalConditions.split('\n').map(item => {
+            return (
+              <li key={nanoid()} className={css.conditionItem}>
+                {item}
+              </li>
+            );
+          })}
+          <li className={css.conditionItem}>
+            Mileage: <span className={css.accentConditions}>{mileage}</span>
+          </li>
+          <li className={css.conditionItem}>
+            Price: <span className={css.accentConditions}>{rentalPrice}</span>
+          </li>
+        </ul>
+        <button id={id} className={css.rentalBtn} type="button">
+          Rental car
+        </button>
+      </div>
+    </div>,
+    modalRoot
+  );
+};
