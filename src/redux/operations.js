e288dd876454a40f8a7ab0cd84ff9624c25f7839
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2023-09-14 23:52:49 +0300

    add fetch params

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -5,7 +5,7 @@ axios.defaults.baseURL = 'https://64881a170e2469c038fcf3d6.mockapi.io';
 
 export const fetchAdverts = createAsyncThunk('adverts/fetchAll', async page => {
   const response = await axios.get('/adverts', {
-    params: { page: page, limit: 2 },
+    params: { page: page, limit: 8 },
   });
   return response.data;
 });
