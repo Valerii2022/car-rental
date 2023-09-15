af8f777b4031d4f0664d1e8eacd1317205c12dbb
Valerii Pometun <vmpometun@gmail.com>
2023-09-15 09:56:53 +0300

    add styles for home

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,21 +3,27 @@ import { NavLink } from 'react-router-dom';
 import { Suspense } from 'react';
 import { ReactComponent as LogoIcon } from '../../image/rental.svg';
 import { ReactComponent as ActiveIcon } from '../../image/active.svg';
+import css from './Layout.module.css';
 
 const Layout = () => {
   return (
     <>
-      <header>
-        <NavLink to="/">
-          <LogoIcon width="32" height="32" />
-          Car rental
-        </NavLink>
-        <NavLink to="/catalog">
-          <p>Catalog</p>
-        </NavLink>
-        <NavLink to="/favourites">
-          <ActiveIcon width="32" height="32" />
-        </NavLink>
+      <header className={css.header}>
+        <div className={css.container}>
+          <NavLink className={css.link} to="/">
+            <LogoIcon width="32" height="32" />
+            Car Rental
+          </NavLink>
+
+          <p className={css.title}>
+            Car Rental – Search, Compare, and Save up to 70%!
+          </p>
+
+          <NavLink className={css.link} to="/favourites">
+            Favourites
+            <ActiveIcon width="32" height="32" />
+          </NavLink>
+        </div>
       </header>
       <main>
         <Suspense fallback={<div>Loading...</div>}>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,20 @@
+import css from './HomePage.module.css';
 import { NavLink } from 'react-router-dom';
 
 export const Home = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/catalog">Catalog</NavLink>
-        </li>
-        <li>
-          <NavLink to="/favourites">Favourites</NavLink>
-        </li>
-      </ul>
+    <div className={css.container}>
+      <h1 className={css.title}>Find Great Deals from Car Rental in Ukraine</h1>
+      <p className={css.subtitle}>
+        We have great offers for daily, weekly and long term rentals in United
+        Kingdom. Find the cheapest prices online, for your future Car Rental in
+        United Kingdom in 4 quick easy steps. Book now at the lowest possible
+        rates only with a small down payment and pay the rest when you pick up
+        the car.
+      </p>
+      <NavLink className={css.link} to="/catalog">
+        Catalog
+      </NavLink>
     </div>
   );
 };
