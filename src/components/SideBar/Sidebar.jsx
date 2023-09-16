699ed6491229a7e3d823fd0c2352b935d3ca9e3c
Valerii Pometun <vmpometun@gmail.com>
2023-09-16 23:27:30 +0300

    add sidebar styles

--- src/components/SideBar/Sidebar.jsx
+++ src/components/SideBar/Sidebar.jsx
@@ -2,7 +2,7 @@ import { useDispatch } from 'react-redux';
 import css from './Sidebar.module.css';
 import React, { useState } from 'react';
 import { addFilters } from 'redux/favouritesSlise';
-// import Select from 'react-select';
+import Select from 'react-select';
 
 export const Sidebar = () => {
   const dispatch = useDispatch();
@@ -12,121 +12,147 @@ export const Sidebar = () => {
 
   const handleSubmitForm = e => {
     e.preventDefault();
-    filters.brand = e.target[0].value;
-    filters.price = Number(e.target[1].value);
+    console.log(e);
+    //  filters.brand = e.target[0].value;
+    //  filters.price = Number(e.target[1].value);
     filters.min = minMileage;
     filters.max = maxMileage;
-    dispatch(addFilters(filters));
+    //  dispatch(addFilters(filters));
     setMaxMileage('');
     setMinMileage('');
   };
 
-  // const brandOptions = [
-  //   { value: 'buick', label: 'Buick' },
-  //   { value: 'volvo', label: 'Volvo' },
-  //   { value: 'hummer', label: 'HUMMER' },
-  //   { value: 'subaru', label: 'Subaru' },
-  //   { value: 'mitsubishi', label: 'Mitsubishi' },
-  //   { value: 'nissan', label: 'Nissan' },
-  //   { value: 'lincoln', label: 'Lincoln' },
-  //   { value: 'gmc', label: 'GMC' },
-  //   { value: 'hyundai', label: 'Hyundai' },
-  //   { value: 'mini', label: 'MINI' },
-  //   { value: 'bentley', label: 'Bentley' },
-  //   { value: 'aston-martin', label: 'Aston Martin' },
-  //   { value: 'pontiac', label: 'Pontiac' },
-  //   { value: 'lamborghini', label: 'Lamborghini' },
-  //   { value: 'audi', label: 'Audi' },
-  //   { value: 'bmw', label: 'BMW' },
-  //   { value: 'chevrolet', label: 'Chevrolet' },
-  //   { value: 'mercedes-Benz', label: 'Mercedes-Benz' },
-  //   { value: 'chrysler', label: 'Chrysler' },
-  //   { value: 'kia', label: 'Kia' },
-  //   { value: 'land', label: 'Land' },
-  // ];
+  const brandOptions = [
+    { value: 'buick', label: 'Buick' },
+    { value: 'volvo', label: 'Volvo' },
+    { value: 'hummer', label: 'HUMMER' },
+    { value: 'subaru', label: 'Subaru' },
+    { value: 'mitsubishi', label: 'Mitsubishi' },
+    { value: 'nissan', label: 'Nissan' },
+    { value: 'lincoln', label: 'Lincoln' },
+    { value: 'gmc', label: 'GMC' },
+    { value: 'hyundai', label: 'Hyundai' },
+    { value: 'mini', label: 'MINI' },
+    { value: 'bentley', label: 'Bentley' },
+    { value: 'aston-martin', label: 'Aston Martin' },
+    { value: 'pontiac', label: 'Pontiac' },
+    { value: 'lamborghini', label: 'Lamborghini' },
+    { value: 'audi', label: 'Audi' },
+    { value: 'bmw', label: 'BMW' },
+    { value: 'chevrolet', label: 'Chevrolet' },
+    { value: 'mercedes-Benz', label: 'Mercedes-Benz' },
+    { value: 'chrysler', label: 'Chrysler' },
+    { value: 'kia', label: 'Kia' },
+    { value: 'land', label: 'Land' },
+    { value: 'lexus', label: 'Lexus' },
+    { value: 'toyota', label: 'Toyota' },
+    { value: 'acura', label: 'Acura' },
+  ];
 
-  // const priceOptions = [
-  //   { value: '30', label: '30' },
-  //   { value: '40', label: '40' },
-  //   { value: '50', label: '50' },
-  //   { value: '60', label: '60' },
-  //   { value: '70', label: '70' },
-  //   { value: '80', label: '80' },
-  //   { value: '90', label: '90' },
-  //   { value: '100', label: '100' },
-  //   { value: '150', label: '150' },
-  //   { value: '200', label: '200' },
-  //   { value: '250', label: '250' },
-  //   { value: '300', label: '300' },
-  //   { value: '350', label: '350' },
-  //   { value: '400', label: '400' },
-  //   { value: '500', label: '500' },
-  //   { value: '1000', label: '1000' },
-  // ];
+  const priceOptions = [
+    { value: '30', label: '30' },
+    { value: '40', label: '40' },
+    { value: '50', label: '50' },
+    { value: '60', label: '60' },
+    { value: '70', label: '70' },
+    { value: '80', label: '80' },
+    { value: '90', label: '90' },
+    { value: '100', label: '100' },
+    { value: '150', label: '150' },
+    { value: '200', label: '200' },
+    { value: '250', label: '250' },
+    { value: '300', label: '300' },
+    { value: '350', label: '350' },
+    { value: '400', label: '400' },
+    { value: '500', label: '500' },
+  ];
+
+  const selectStyles = {
+    control: styles => ({
+      borderRadius: '14px',
+      backgroundColor: '#f7f7fb',
+      height: '48px',
+      display: 'flex',
+      padding: '0',
+      paddingLeft: '10px',
+      paddingRight: '18px',
+      borderRight: 'none',
+      color: '#121417',
+      fontSize: '18px',
+      fontWeight: '500',
+      lineHeight: '1.11',
+    }),
+    option: (styles, { isFocused }) => {
+      return {
+        ...styles,
+        backgroundColor: 'transparent',
+        color: isFocused ? '#121417' : 'rgba(18, 20, 23, 0.20)',
+        fontSize: '16px',
+        fontWeight: '500',
+        lineHeight: '1.25',
+        cursor: 'pointer',
+        paddingLeft: '0',
+        paddingRight: '0',
+        paddingTop: '4px',
+        paddingBottom: '4px',
+      };
+    },
+    placeholder: defaultStyles => {
+      return {
+        ...defaultStyles,
+        color: '#121417',
+      };
+    },
+    dropdownIndicator: (provided, state) => ({
+      ...provided,
+      svg: {
+        fill: '#121417',
+      },
+      transition: 'transform 250ms linear',
+      transform: state.isFocused ? 'rotate(180deg)' : null,
+    }),
+    menu: provided => ({
+      ...provided,
+      borderRadius: '10px',
+      paddingLeft: '18px',
+      paddingTop: '10px',
+      paddingBottom: '18px',
+      paddingRight: '8px',
+    }),
+    menuList: provided => ({
+      ...provided,
+      color: 'red',
+    }),
+  };
 
   return (
     <div className={css.container}>
       <form className={css.form} onSubmit={handleSubmitForm}>
         <div>
           <p className={css.selectTitle}>Car brand</p>
-          <select className={css.selectField}>
-            <option value="Enter the text">Enter the text</option>
-            <option value="Buick">Buick</option>
-            <option value="Volvo">Volvo</option>
-            <option value="HUMMER">HUMMER</option>
-            <option value="Subaru">Subaru</option>
-            <option value="Mitsubishi">Mitsubishi</option>
-            <option value="Nissan">Nissan</option>
-            <option value="Lincoln">Lincoln</option>
-            <option value="GMC">GMC</option>
-            <option value="Hyundai">Hyundai</option>
-            <option value="MINI">MINI</option>
-            <option value="Bentley">Bentley</option>
-            <option value="Aston Martin">Aston Martin</option>
-            <option value="Pontiac">Pontiac</option>
-            <option value="Lamborghini">Lamborghini</option>
-            <option value="Audi">Audi</option>
-            <option value="BMW">BMW</option>
-            <option value="Chevrolet">Chevrolet</option>
-            <option value="Mercedes-Benz">Mercedes-Benz</option>
-            <option value="Chrysler">Chrysler</option>
-            <option value="Kia">Kia</option>
-            <option value="Land">Land</option>
-            <option value="Toyota">Toyota</option>
-            <option value="Lexus">Lexus</option>
-            <option value="Acura">Acura</option>
-          </select>
-          {/* <Select
+          <Select
+            maxMenuHeight={272}
             className={css.selectBrandField}
             placeholder="Enter the text"
             options={brandOptions}
-          /> */}
+            styles={selectStyles}
+            components={{
+              IndicatorSeparator: () => null,
+            }}
+          />
         </div>
         <div>
           <p className={css.selectTitle}>Price / 1 hour</p>
-          <select style={{ width: 125 }} className={css.selectField}>
-            <option value="default">To $</option>
-            <option value="30">30</option>
-            <option value="40">40</option>
-            <option value="50">50</option>
-            <option value="60">60</option>
-            <option value="70">70</option>
-            <option value="80">80</option>
-            <option value="90">90</option>
-            <option value="100">100</option>
-            <option value="150">150</option>
-            <option value="200">200</option>
-            <option value="250">250</option>
-            <option value="300">300</option>
-            <option value="400">400</option>
-            <option value="500">500</option>
-            <option value="600">600</option>
-          </select>
-          {/* <Select
+          <Select
+            maxMenuHeight={188}
             className={css.selectPriceField}
             placeholder="To $"
             options={priceOptions}
-          /> */}
+            styles={selectStyles}
+            components={{
+              IndicatorSeparator: () => null,
+            }}
+          />
         </div>
         <div>
           <p className={css.selectTitle}>Car mileage / km</p>
